06f714dd40785590c8eadb7ee5ed2c2503d72103
juanandres-montero <juanandres.montero@outlook.com>
2024-03-27 11:25:38 -0600

    commit

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   // used to generate images
   site:
     process.env.VERCEL_ENV === 'production'
-      ? 'https://juanandres-montero.github.io/opcr'
+      ? 'https://brutal.elian.codes/'
       : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/`
       : 'https://localhost:3000/',
