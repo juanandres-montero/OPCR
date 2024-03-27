b7f9cf1428fd358e223dd086f5890902f898aa2e
juanandres-montero <juanandres.montero@outlook.com>
2024-03-27 14:37:48 -0600

    astro fonts

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -12,7 +12,7 @@ export async function GET(context) {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/blog/${post.slug}/`,
+      link: `/OPCR/blog/${post.slug}/`,
     })),
     customData: '<language>es-cr</language>',
     canonicalUrl: 'https://juanandres-montero.github.io/opcr/',
