b27301d4aaec99aaca8a35f9d462b702eb2bc3ed
juanandres-montero <juanandres.montero@outlook.com>
2024-03-27 13:24:25 -0600

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,4 @@ export default defineConfig({
   // used to generate images
   site: 'https://juanandres-montero.github.io',
   base: 'OPCR',
-  trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
-  vite: {
-    optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
-    },
-  },
 });
